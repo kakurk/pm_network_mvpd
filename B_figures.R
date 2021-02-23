517f8826cd69a1158783862275eabd9f1b129b44
kylekurkela <kurkela@bc.edu>
2021-02-23 18:19:12 -0500

    updated figure with PC23

--- B_figures.R
+++ B_figures.R
@@ -1,14 +1,24 @@
 # Analyze the homogeneity of our ROIs
 
 library(tidyverse)
+library(magrittr)
 
+# load
 df <- read_csv(file = 'roi_homogeneity.csv', col_types = cols())
 
+# tidy
+df %<>% 
+  pivot_longer(cols = var_expl_pc1:var_expl_pc23, 
+               names_to = 'PC', 
+               values_to = 'var_expl', 
+               names_prefix = 'var_expl_')
+  
+# figure
 ggplot(data = df, mapping = aes(x = roi, y = var_expl, group = subject)) +
   stat_summary(geom = 'point', fun.data = 'mean_se', alpha = .4) +
   stat_summary(geom = 'line', fun.data = 'mean_se', alpha = .2) +
   stat_summary(aes(group = NULL), geom = 'crossbar', fun.data = mean_se, width = .2, fill = 'gray') +
-  labs(x = 'ROI', y = 'Homogeneity',
-       caption = 'Homoggeneity = % variance explained by first PC') -> p
+  facet_grid(.~PC) +
+  labs(x = 'ROI', y = 'Proportion Variance Explained') -> p
 
 print(p)
